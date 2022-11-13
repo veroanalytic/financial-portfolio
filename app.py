c9278b40b4790640fb4d9cc41c914d95e4dffe85
veroanalytic <veroanalytic@gmail.com>
2022-11-13 02:42:44 -0500

    split stocks into different csv's

--- app.py
+++ app.py
@@ -1,187 +1,83 @@
+# Import Libraries
 import pandas as pd
 import yfinance as yf
 import streamlit as st
-
 import os
+from time import sleep
 
 pwd = os.getcwd()
 
-# =======================================================================================================================================
-#                                                    MCD
-# =======================================================================================================================================
 
-mcd = yf.Ticker("mcd")
-mcd_history = mcd.history(period="max")
+def ticker_function(ticker_symbol):
 
-# Create Ticker Column
-mcd_history["Ticker"] = "MCD"
+    # Call yfinance ticker info
+    ticker = yf.Ticker(ticker_symbol)
 
-# Create rolling weekly average column
-mcd_history["weekly_avg_close"] = mcd_history["Close"].rolling(7).mean()
+    # Assign variable for ticker history
+    ticker_history = ticker.history(period="max")
 
-# Create weekly average close percent column
-mcd_history["weekly_avg_close_percent"] = (mcd_history["Close"] - mcd_history["weekly_avg_close"]) / mcd_history["Close"] * 100
+    # Number of rows displayed
+    rows_returned = 5
 
-# Create rolling monthly average column
-mcd_history["monthly_avg_close"] = mcd_history["Close"].rolling(30).mean()
+    # Create derived columns
+    ticker_history["Ticker"] = ticker_symbol
+    ticker_history["Wkly_Avg_Close"] = ticker_history["Close"].rolling(7).mean()
+    ticker_history["Wkly_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Wkly_Avg_Close"]) / ticker_history["Close"] * 100
+    ticker_history["Monthly_Avg_Close"] = ticker_history["Close"].rolling(30).mean()
+    ticker_history["Monthly_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Monthly_Avg_Close"]) / ticker_history["Close"] * 100
+    ticker_history["Semi_Annual_Avg_Close"] = ticker_history["Close"].rolling(183).mean()
+    ticker_history["Semi_Annual_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Semi_Annual_Avg_Close"]) / ticker_history["Close"] * 100
+    ticker_history["Annual_Avg_Close"] = ticker_history["Close"].rolling(365).mean()
+    ticker_history["Annual_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Annual_Avg_Close"]) / ticker_history["Close"] * 100
+    ticker_history["Two_Year_Avg_Close"] = ticker_history["Close"].rolling(730).mean()
+    ticker_history["Two_Year_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Two_Year_Avg_Close"]) / ticker_history["Close"] * 100
 
-# Create monthly average close percent column
-mcd_history["monthly_avg_close_percent"] = (mcd_history["Close"] - mcd_history["monthly_avg_close"]) / mcd_history["Close"] * 100
+    # Drop NAs
+    ticker_history = ticker_history.dropna()
 
-# Create rolling semi_annual average column
-mcd_history["semi_annual_avg_close"] = mcd_history["Close"].rolling(183).mean()
+    # Create new DF of only needed columns
+    ticker_price_action = ticker_history[["Ticker", "Close", 
+                                    "Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
+                                    "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent",
+                                    "Wkly_Avg_Close", "Monthly_Avg_Close", 
+                                    "Semi_Annual_Avg_Close", "Annual_Avg_Close", "Two_Year_Avg_Close"
+                                    ]] \
+                                    .sort_values(by=["Date"], ascending=False) \
+                                    .head(rows_returned)
+    ticker_price_action = ticker_price_action.reset_index()
 
-# Create semi_annual average close percent column
-mcd_history["semi_annual_avg_close_percent"] = (mcd_history["Close"] - mcd_history["semi_annual_avg_close"]) / mcd_history["Close"] * 100
+    ticker_price_action["Date"] = ticker_price_action["Date"].dt.date
 
-# Create rolling annual average column
-mcd_history["annual_avg_close"] = mcd_history["Close"].rolling(365).mean()
+    return ticker_price_action
 
-# Create annual average close percent column
-mcd_history["annual_avg_close_percent"] = (mcd_history["Close"] - mcd_history["annual_avg_close"]) / mcd_history["Close"] * 100
-
-
-# Create rolling two_year average column
-mcd_history["two_year_avg_close"] = mcd_history["Close"].rolling(730).mean()
-
-# Create two_year average close percent column
-mcd_history["two_year_avg_close_percent"] = (mcd_history["Close"] - mcd_history["two_year_avg_close"]) / mcd_history["Close"] * 100
-
-# Drop NAs
-mcd_history = mcd_history.dropna()
-
-# Grab only needed columns
-mcd_price_action = mcd_history[["Ticker", "Close", 
-                                "weekly_avg_close_percent", "monthly_avg_close_percent",
-                                "semi_annual_avg_close_percent", "annual_avg_close_percent", "two_year_avg_close_percent",
-                                "weekly_avg_close", "monthly_avg_close", 
-                                "semi_annual_avg_close", "annual_avg_close", "two_year_avg_close"
-                                ]] \
-                                .sort_values(by=["Date"], ascending=False) \
-                                .head(5)
-mcd_price_action = mcd_price_action.reset_index()
+# Assign function call to ticker
+mcd_ticker = ticker_function("MCD")
+pep_ticker = ticker_function("PEP")
+msft_ticker = ticker_function("MSFT")
 
 # Export to CSV
-mcd_price_action.to_csv(pwd + "\\stocks.csv", mode ="w", header=True, index=False)
+mcd_ticker.to_csv(pwd + "\\mcd_stocks.csv", mode ="w", header=True, index=False)
+pep_ticker.to_csv(pwd + "\\pep_stocks.csv", mode ="w", header=True, index=False)
+msft_ticker.to_csv(pwd + "\\msft_stocks.csv", mode ="w", header=True, index=False)
+
+# Create CSV data frame the import into Streamlit
+mcd_df = pd.read_csv(pwd + "\\mcd_stocks.csv")
+pep_df = pd.read_csv(pwd + "\\pep_stocks.csv")
+msft_df = pd.read_csv(pwd + "\\msft_stocks.csv")
 
 
-# =======================================================================================================================================
-#                                                    PEP
-# =======================================================================================================================================
-pep = yf.Ticker("PEP")
-pep_history = pep.history(period="max")
 
-# Create Ticker Column
-pep_history["Ticker"] = "PEP"
+# Run Streamlit
+st.set_page_config(layout="wide")
+st.title("Weekly DCA Assessment")
+st.subheader("McDonald's Corp")
+st.write(mcd_df)
+st.subheader("PepsiCo, Inc.")
+st.write(pep_df)
+st.subheader("Microsoft Corp")
+st.write(msft_df)
 
-# Create rolling weekly average column
-pep_history["weekly_avg_close"] = pep_history["Close"].rolling(7).mean()
+# add_sidebar = st.sidebar.selectbox("Test Side Bar Title", ("Test 1", "Test 2"))
 
-# Create weekly average close percent column
-pep_history["weekly_avg_close_percent"] = (pep_history["Close"] - pep_history["weekly_avg_close"]) / pep_history["Close"] * 100
-
-# Create rolling monthly average column
-pep_history["monthly_avg_close"] = pep_history["Close"].rolling(30).mean()
-
-# Create monthly average close percent column
-pep_history["monthly_avg_close_percent"] = (pep_history["Close"] - pep_history["monthly_avg_close"]) / pep_history["Close"] * 100
-
-# Create rolling semi_annual average column
-pep_history["semi_annual_avg_close"] = pep_history["Close"].rolling(183).mean()
-
-# Create semi_annual average close percent column
-pep_history["semi_annual_avg_close_percent"] = (pep_history["Close"] - pep_history["semi_annual_avg_close"]) / pep_history["Close"] * 100
-
-# Create rolling annual average column
-pep_history["annual_avg_close"] = pep_history["Close"].rolling(365).mean()
-
-# Create annual average close percent column
-pep_history["annual_avg_close_percent"] = (pep_history["Close"] - pep_history["annual_avg_close"]) / pep_history["Close"] * 100
-
-# Create rolling two_year average column
-pep_history["two_year_avg_close"] = pep_history["Close"].rolling(730).mean()
-
-# Create two_year average close percent column
-pep_history["two_year_avg_close_percent"] = (pep_history["Close"] - pep_history["two_year_avg_close"]) / pep_history["Close"] * 100
-
-
-# Drop NAs
-pep_history = pep_history.dropna()
-
-# Grab only needed columns
-pep_price_action = pep_history[["Ticker", "Close", 
-                                "weekly_avg_close_percent", "monthly_avg_close_percent",
-                                "semi_annual_avg_close_percent", "annual_avg_close_percent", "two_year_avg_close_percent",
-                                "weekly_avg_close", "monthly_avg_close", 
-                                "semi_annual_avg_close", "annual_avg_close", "two_year_avg_close"
-                                ]] \
-                                .sort_values(by=["Date"], ascending=False) \
-                                .head(5)
-pep_price_action = pep_price_action.reset_index()
-
-
-# Export to CSV
-pep_price_action.to_csv(pwd + "\\stocks.csv", mode ="a", header=False, index=False)
-
-# =======================================================================================================================================
-#                                                    MSFT
-# =======================================================================================================================================
-
-msft = yf.Ticker("MSFT")
-msft_history = msft.history(period="max")
-
-msft_history["Ticker"] = "MSFT"
-
-# Create rolling weekly average column
-msft_history["weekly_avg_close"] = msft_history["Close"].rolling(7).mean()
-
-# Create weekly average close percent column
-msft_history["weekly_avg_close_percent"] = (msft_history["Close"] - msft_history["weekly_avg_close"]) / msft_history["Close"] * 100
-
-# Create rolling monthly average column
-msft_history["monthly_avg_close"] = msft_history["Close"].rolling(30).mean()
-
-# Create monthly average close percent column
-msft_history["monthly_avg_close_percent"] = (msft_history["Close"] - msft_history["monthly_avg_close"]) / msft_history["Close"] * 100
-
-# Create rolling semi_annual average column
-msft_history["semi_annual_avg_close"] = msft_history["Close"].rolling(183).mean()
-
-# Create semi_annual average close percent column
-msft_history["semi_annual_avg_close_percent"] = (msft_history["Close"] - msft_history["semi_annual_avg_close"]) / msft_history["Close"] * 100
-
-# Create rolling annual average column
-msft_history["annual_avg_close"] = msft_history["Close"].rolling(365).mean()
-
-# Create annual average close percent column
-msft_history["annual_avg_close_percent"] = (msft_history["Close"] - msft_history["annual_avg_close"]) / msft_history["Close"] * 100
-
-# Create rolling two_year average column
-msft_history["two_year_avg_close"] = msft_history["Close"].rolling(730).mean()
-
-# Create two_year average close percent column
-msft_history["two_year_avg_close_percent"] = (msft_history["Close"] - msft_history["two_year_avg_close"]) / msft_history["Close"] * 100
-
-
-# Drop NAs
-msft_history = msft_history.dropna()
-
-# Grab only needed columns
-msft_price_action = msft_history[["Ticker", "Close", 
-                                "weekly_avg_close_percent", "monthly_avg_close_percent",
-                                "semi_annual_avg_close_percent", "annual_avg_close_percent", "two_year_avg_close_percent",
-                                "weekly_avg_close", "monthly_avg_close", 
-                                "semi_annual_avg_close", "annual_avg_close", "two_year_avg_close"
-                                ]] \
-                                .sort_values(by=["Date"], ascending=False) \
-                                .head(5)
-msft_price_action = msft_price_action.reset_index()
-
-# Export to CSV
-msft_price_action.to_csv(pwd + "\\stocks.csv", mode ="a", header=False, index=False)
-
-
-df = pd.read_csv(pwd + "\\stocks.csv")
-
-st.title("Hello world!")
-st.write(df)
+# Run in Terminal Window
+# "streamlit run c:/Users/jisbe/pyproject/financial-portfolio/app.py"
