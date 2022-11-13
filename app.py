7f68ebda342158a5787bb69e2d237aa7c836c2bd
Jason <veroanalytic@gmail.com>
2022-11-13 13:56:59 -0500

    WIP on daily to previous change comparison

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from time import sleep
 pwd = os.getcwd()
 
 
-def ticker_function(ticker_symbol):
+def ticker_history(ticker_symbol):
 
     # Call yfinance ticker info
     ticker = yf.Ticker(ticker_symbol)
@@ -16,27 +16,33 @@ def ticker_function(ticker_symbol):
     # Assign variable for ticker history
     ticker_history = ticker.history(period="max")
 
+    return ticker_history
+
+def ticker_price_action(ticker_symbol):
+    
+    ticker_price_action = ticker_history(ticker_symbol)
+
     # Number of rows displayed
     rows_returned = 5
 
     # Create derived columns
-    ticker_history["Ticker"] = ticker_symbol
-    ticker_history["Wkly_Avg_Close"] = ticker_history["Close"].rolling(7).mean()
-    ticker_history["Wkly_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Wkly_Avg_Close"]) / ticker_history["Close"] * 100
-    ticker_history["Monthly_Avg_Close"] = ticker_history["Close"].rolling(30).mean()
-    ticker_history["Monthly_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Monthly_Avg_Close"]) / ticker_history["Close"] * 100
-    ticker_history["Semi_Annual_Avg_Close"] = ticker_history["Close"].rolling(183).mean()
-    ticker_history["Semi_Annual_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Semi_Annual_Avg_Close"]) / ticker_history["Close"] * 100
-    ticker_history["Annual_Avg_Close"] = ticker_history["Close"].rolling(365).mean()
-    ticker_history["Annual_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Annual_Avg_Close"]) / ticker_history["Close"] * 100
-    ticker_history["Two_Year_Avg_Close"] = ticker_history["Close"].rolling(730).mean()
-    ticker_history["Two_Year_Avg_Close_Percent"] = (ticker_history["Close"] - ticker_history["Two_Year_Avg_Close"]) / ticker_history["Close"] * 100
+    ticker_price_action["Ticker"] = ticker_symbol
+    ticker_price_action["Wkly_Avg_Close"] = ticker_price_action["Close"].rolling(7).mean()
+    ticker_price_action["Wkly_Avg_Close_Percent"] = (ticker_price_action["Close"] - ticker_price_action["Wkly_Avg_Close"]) / ticker_price_action["Close"] * 100
+    ticker_price_action["Monthly_Avg_Close"] = ticker_price_action["Close"].rolling(30).mean()
+    ticker_price_action["Monthly_Avg_Close_Percent"] = (ticker_price_action["Close"] - ticker_price_action["Monthly_Avg_Close"]) / ticker_price_action["Close"] * 100
+    ticker_price_action["Semi_Annual_Avg_Close"] = ticker_price_action["Close"].rolling(183).mean()
+    ticker_price_action["Semi_Annual_Avg_Close_Percent"] = (ticker_price_action["Close"] - ticker_price_action["Semi_Annual_Avg_Close"]) / ticker_price_action["Close"] * 100
+    ticker_price_action["Annual_Avg_Close"] = ticker_price_action["Close"].rolling(365).mean()
+    ticker_price_action["Annual_Avg_Close_Percent"] = (ticker_price_action["Close"] - ticker_price_action["Annual_Avg_Close"]) / ticker_price_action["Close"] * 100
+    ticker_price_action["Two_Year_Avg_Close"] = ticker_price_action["Close"].rolling(730).mean()
+    ticker_price_action["Two_Year_Avg_Close_Percent"] = (ticker_price_action["Close"] - ticker_price_action["Two_Year_Avg_Close"]) / ticker_price_action["Close"] * 100
 
     # Drop NAs
-    ticker_history = ticker_history.dropna()
+    ticker_price_action = ticker_price_action.dropna()
 
     # Create new DF of only needed columns
-    ticker_price_action = ticker_history[["Ticker", "Close", 
+    ticker_price_action = ticker_price_action[["Ticker", "Close", 
                                     "Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
                                     "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent",
                                     "Wkly_Avg_Close", "Monthly_Avg_Close", 
@@ -51,26 +57,32 @@ def ticker_function(ticker_symbol):
     return ticker_price_action
 
 # Assign function call to ticker
-mcd_ticker = ticker_function("MCD")
-pep_ticker = ticker_function("PEP")
-msft_ticker = ticker_function("MSFT")
+mcd_ticker = ticker_price_action("MCD")
+pep_ticker = ticker_price_action("PEP")
+msft_ticker = ticker_price_action("MSFT")
+o_ticker = ticker_price_action("O")
 
 # Export to CSV
 mcd_ticker.to_csv(pwd + "\\mcd_stocks.csv", mode ="w", header=True, index=False)
 pep_ticker.to_csv(pwd + "\\pep_stocks.csv", mode ="w", header=True, index=False)
 msft_ticker.to_csv(pwd + "\\msft_stocks.csv", mode ="w", header=True, index=False)
+o_ticker.to_csv(pwd + "\\o_stocks.csv", mode ="w", header=True, index=False)
 
 # Create CSV data frame the import into Streamlit
 mcd_df = pd.read_csv(pwd + "\\mcd_stocks.csv")
 pep_df = pd.read_csv(pwd + "\\pep_stocks.csv")
 msft_df = pd.read_csv(pwd + "\\msft_stocks.csv")
+o_df = pd.read_csv(pwd + "\\o_stocks.csv")
 
 
+# Run Streamlit
+
+# Streamlit conditional function
 def percent_variance(val):
     color = "red" if val < 0 else "green" # if val > 0 else "green"
     return f"background-color: {color}"
 
-# Run Streamlit
+
 st.set_page_config(layout="wide")
 st.title("Weekly DCA Assessment")
 
@@ -89,6 +101,12 @@ st.dataframe(pep_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Per
 st.subheader("Microsoft Corp")
 # st.write(msft_df)
 st.dataframe(msft_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
+                                                             "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent"
+                                                             ]))
+
+st.subheader("Realty Income Corp")
+# st.write(o_df)
+st.dataframe(o_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
                                                              "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent"
                                                              ]))
 
