8b0ae928508d151abe3e5e46a8c37409218a3055
veroanalytic <veroanalytic@gmail.com>
2022-11-13 10:56:07 -0500

    added conditional colors to streamlit dfs

--- app.py
+++ app.py
@@ -66,15 +66,33 @@ pep_df = pd.read_csv(pwd + "\\pep_stocks.csv")
 msft_df = pd.read_csv(pwd + "\\msft_stocks.csv")
 
 
+def percent_variance(val):
+    color = "red" if val < 0 else "green" # if val > 0 else "green"
+    return f"background-color: {color}"
+
+
+
 
 # Run Streamlit
 st.set_page_config(layout="wide")
 st.title("Weekly DCA Assessment")
+
 st.subheader("McDonald's Corp")
-st.write(mcd_df)
+# st.write(mcd_df)
+st.dataframe(mcd_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
+                                                             "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent"
+                                                             ]))
+
 st.subheader("PepsiCo, Inc.")
-st.write(pep_df)
+# st.write(pep_df)
+st.dataframe(pep_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
+                                                             "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent"
+                                                             ]))
+
 st.subheader("Microsoft Corp")
-st.write(msft_df)
+# st.write(msft_df)
+st.dataframe(msft_df.style.applymap(percent_variance, subset=["Wkly_Avg_Close_Percent", "Monthly_Avg_Close_Percent",
+                                                             "Semi_Annual_Avg_Close_Percent", "Annual_Avg_Close_Percent", "Two_Year_Avg_Close_Percent"
+                                                             ]))
 
 # add_sidebar = st.sidebar.selectbox("Test Side Bar Title", ("Test 1", "Test 2"))
